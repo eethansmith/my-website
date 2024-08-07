33b26d915afb99273cc8b04ab96f89699c30f3c5
psyes9 <ethan.a.smith@hotmail.co.uk>
2024-08-08 00:07:59 +0100

    vector index made

--- backend/chatgpt.py
+++ backend/chatgpt.py
@@ -2,7 +2,12 @@ import os
 import sys
 
 import constants
+from langchain.document_loader import TextLoader
+from langchain.indexes import VectorstoreIndexCreator
 
 os.environ["OPENAI_API_KEY"] = constants.APIKEY
 
 query = sys.argv[1]
+print(query)
+
+loader = TextLoader('data.txt')
