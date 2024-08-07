9f556e89c291d8b672e336c663f4e2e9343d3df0
psyes9 <ethan.a.smith@hotmail.co.uk>
2024-08-08 00:14:38 +0100

    changes made to index vector

--- backend/chatgpt.py
+++ backend/chatgpt.py
@@ -2,16 +2,16 @@ import os
 import sys
 
 import constants
-from langchain.document_loader import TextLoader
+from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI 
 
 os.environ["OPENAI_API_KEY"] = constants.APIKEY
 
 query = sys.argv[1]
-print(query)
 
 loader = TextLoader('data.txt')
-
 index = VectorstoreIndexCreator().from_loaders([loader])
 
 print (index.query(query))
