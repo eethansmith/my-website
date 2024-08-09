f4854bee39cbee6320a163b332210aa6c5003eba
psyes9 <ethan.a.smith@hotmail.co.uk>
2024-08-09 23:02:58 +0100

    added fading background and font colour

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import me from './menu-con.jpg';
 import CyberFirstLogo from './CyberFirst_logo.jpg';
 import NottinghamLogo from './Nottingham_logo.jpg';
@@ -9,10 +9,41 @@ import QinetiQLogo from './QinetiQ_logo.jpg';
 import './App.css';
 
 function App() {
+  const [scrollY, setScrollY] = useState(0);
+
+  const handleScroll = () => {
+    setScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const interpolateColor = (startColor, endColor, factor) => {
+    let result = startColor.slice();
+    for (let i = 0; i < 3; i++) {
+      result[i] = Math.round(result[i] + factor * (endColor[i] - startColor[i]));
+    }
+    return `rgb(${result.join(',')})`;
+  };
+
+  const startColor = [26, 26, 26]; // #1a1a1a in RGB
+  const endColor = [250, 250, 250]; // #fafafa in RGB
+  const startFontColor = [250, 250, 250]; // #fafafa in RGB
+  const endFontColor = [0, 0, 0]; // #000000 in RGB
+  const factor = Math.min(scrollY / 600, 1); // adjust the denominator for sensitivity
+
+  const backgroundColor = interpolateColor(startColor, endColor, factor);
+  const fontColor = interpolateColor(startFontColor, endFontColor, factor);
+
   return (
-    <div className="app">
+    <div className="app" style={{ backgroundColor, color: fontColor }}>
       <header className="header">
-      <div className="brand">
+        <div className="brand">
           eethansmith
         </div>
         <nav className="nav">
@@ -45,8 +76,10 @@ function App() {
           <img src={me} alt="Ethan Smith" className="profile-image" />
           <h1>Ethan Smith</h1>
           <p className="education">Computer Science at the University of Nottingham</p>
+
           <div className="logo-section">
             <img src={CyberFirstLogo} alt="CyberFirst" className="logo" />
+            <img src={NottinghamLogo} alt="Nottingham" className="logo" />
             <img src={BTLogo} alt="BT" className="logo" />
             <img src={MicrosoftLogo} alt="Microsoft" className="logo" />
             <img src={QinetiQLogo} alt="QinetiQ" className="logo" />
